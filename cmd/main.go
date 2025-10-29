cbd322a216d37706f44b1e40ee1508d182ca86c0
Zach Leslie <xaque208@gmail.com>
2025-10-29 23:21:24 +0000

    Initial BastillJail types and controller

--- cmd/main.go
+++ cmd/main.go
@@ -37,6 +37,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	"github.com/go-logr/logr"
+
 	commonv1 "github.com/zachfi/nodemanager/api/common/v1"
 	controller "github.com/zachfi/nodemanager/internal/controller/common"
 
@@ -46,6 +47,7 @@ import (
 
 	// "github.com/zachfi/nodemanager/pkg/nodes/freebsd"
 
+	freebsdcontroller "github.com/zachfi/nodemanager/internal/controller/freebsd"
 	//+kubebuilder:scaffold:imports
 	"github.com/zachfi/zkit/pkg/tracing"
 )
@@ -205,6 +207,13 @@ func main() {
 	// 	}
 	// }
 
+	if err := (&freebsdcontroller.BastilleJailReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "BastilleJail")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err = mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
